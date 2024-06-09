56caaa3d45eb18054a06099d2e3e792822978faa
Sergey <s.davydov.oh@gmail.com>
2024-06-10 00:51:01 +0800

    Feat: Add logging (one handler for CLI messages, another for the file)

--- simple_downloader/__main__.py
+++ simple_downloader/__main__.py
@@ -1,5 +1,19 @@
+import logging.config
+import sys
+
+from simple_downloader.core.logging_settings import LOGGING
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger("simple_downloader")
+
+
 def main() -> None: ...
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception:
+        logger.exception("There was an unexpected error")
+        logger.info("[?] Unknown Error: Please report it to the developer")
+        sys.exit(1)
---
+++ simple_downloader/core/logging_settings.py
@@ -0,0 +1,59 @@
+from logging import Filter, LogRecord
+
+from simple_downloader.config import BASE_DIR
+
+
+class ConsoleFilter(Filter):
+    def filter(self, record: LogRecord) -> bool:
+        return record.levelname == "INFO"
+
+
+class FileFilter(Filter):
+    def filter(self, record: LogRecord) -> bool:
+        return record.levelname != "INFO"
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console_format": {
+            "format": "{message}.",
+            "style": "{",
+        },
+        "file_format": {
+            "format": "{asctime} | {levelname} | {module}:{funcName}:{lineno} - {message}.",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "console_format",
+            "filters": ["console_filter"],
+        },
+        "file": {
+            "level": "WARNING",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR.joinpath("debug.log"),
+            "formatter": "file_format",
+            "filters": ["file_filter"],
+        },
+    },
+    "loggers": {
+        "simple_downloader": {
+            "level": "INFO",
+            "handlers": ["file", "console"],
+        },
+    },
+    "filters": {
+        "console_filter": {
+            "()": ConsoleFilter,
+        },
+        "file_filter": {
+            "()": FileFilter,
+        },
+    },
+}
