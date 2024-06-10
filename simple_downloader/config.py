dc079bab8795eaf49e43a4d21c94aed336002830
Sergey <s.davydov.oh@gmail.com>
2024-06-10 20:27:12 +0800

    Feat: Add downloader

--- simple_downloader/config.py
+++ simple_downloader/config.py
@@ -5,3 +5,4 @@ SAVE_FOLDER_NAME = "saves"
 DELAY: float | tuple[float, float] | None = (1.1, 3.1)
 MAX_REDIRECTS = 3
 TIMEOUT: tuple[float, float] = (3.03, 42)  # connect and read timeout.
+CHUNK_MULTIPLIER = 8
---
+++ simple_downloader/handlers/downloader.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from logging import getLogger
+
+from simple_downloader.config import CHUNK_MULTIPLIER
+from simple_downloader.core.models import MediaFile
+from simple_downloader.handlers.requester import requester
+
+logger = getLogger(__name__)
+
+
+def download(
+    file: MediaFile,
+    save_path: Path,
+    chunk_size: int = 1024 * CHUNK_MULTIPLIER,
+) -> None:
+    stream = requester(file.stream_url, stream=True)
+
+    def save() -> None:
+        try:
+            bf_out = save_path.joinpath(str(file.filename)).open("bw")
+        except IOError as e:
+            logger.debug(e)
+        else:
+            logger.debug("Downloading %s", file.url)
+            with bf_out:
+                for chunk in stream.iter_content(chunk_size):
+                    bf_out.write(chunk)
+            logger.debug("Downloaded %s", file.name)
+
+    return save()
