f7a2d5f9134bb2aa0b54225f9b6bd5d23932b6d3
Sergey <s.davydov.oh@gmail.com>
2024-06-11 19:06:20 +0800

    Fix: Remove all forbidden chars for the dir name and filename

--- simple_downloader/core/utils.py
+++ simple_downloader/core/utils.py
@@ -4,7 +4,9 @@ from yarl import URL
 from simple_downloader.core.exceptions import ExtensionNotFound
 from simple_downloader.core.models import Extension, Filename
 
+
 FILENAME = compile(r"(.*)(\.\w+$)")
+FORBIDDEN = '/<>:"\\|?*'  # https://en.wikipedia.org/wiki/Filename#Reserved_characters_and_words
 
 
 def get_url_from_args(arguments: tuple) -> URL | str:
@@ -19,6 +21,13 @@ def parse_filename(name: str) -> Filename:
     match = FILENAME.search(name)
     if match is not None:
         stem, ext = match.groups()
-        return Filename(stem, Extension(ext))
+        return Filename(sanitize(stem), Extension(ext))
 
     raise ExtensionNotFound(f'File "{name}" doesn\'t have an extension')
+
+
+def sanitize(name: str, separator: str = "_") -> str:
+    """Removes all forbidden chars for the dir name and filename."""
+
+    name = "".join(separator if char in FORBIDDEN else char for char in name)
+    return name.rstrip(".").strip()
