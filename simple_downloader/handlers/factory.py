7075169153e25b4c6dcc13affddbc78a4c623ac0
Sergey <s.davydov.oh@gmail.com>
2024-07-04 14:41:08 +0800

    Refactor: Remove the closure

--- simple_downloader/handlers/factory.py
+++ simple_downloader/handlers/factory.py
@@ -1,5 +1,5 @@
 from logging import getLogger
-from re import search
+from re import compile
 
 from yarl import URL
 
@@ -12,19 +12,21 @@ from simple_downloader.handlers.requester import Requester
 logger = getLogger(__name__)
 
 HOST_MAPPING = {
-    "cyberdrop": crawlers.Cyberdrop,
-    "bunkr": crawlers.Bunkr,
+    compile(r"cyberdrop"): crawlers.Cyberdrop,
+    compile(r"bunkr"): crawlers.Bunkr,
 }
 
 
 def get_crawler(url: URL, http_client: Requester) -> Crawler:
-    def choice_crawler() -> type[Crawler]:
-        if url.host is not None:
-            for host_pattern, crawler in HOST_MAPPING.items():
-                if search(host_pattern, url.host):
-                    logger.debug("Received <%s> crawler for %s", crawler.__module__, url)
-                    return crawler
+    crawler = _choice_crawler(url)
+    return crawler(url.origin(), http_client)
 
-        raise CrawlerNotFound(url)
 
-    return choice_crawler()(url.origin(), http_client)
+def _choice_crawler(url: URL) -> type[Crawler]:
+    if url.host is not None:
+        for host_pattern, crawler in HOST_MAPPING.items():
+            if host_pattern.search(url.host):
+                logger.debug("Received <%s> crawler for %s", crawler.__module__, url)
+                return crawler
+
+    raise CrawlerNotFound(url)
