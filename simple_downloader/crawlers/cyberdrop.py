e36876e6f2c49b32374ae3bd6730232c85dae960
Sergey <s.davydov.oh@gmail.com>
2024-07-05 23:47:59 +0800

    Refactor: Break down the Cyberdrop into smaller parts to reduce its complexity

--- simple_downloader/crawlers/cyberdrop.py
+++ simple_downloader/crawlers/cyberdrop.py
@@ -9,23 +9,32 @@ class Cyberdrop(Crawler):
     BASE_API = URL("https://cyberdrop.me/api")
 
     def scrape_media(self, url: URL) -> MediaAlbum | MediaFile:
+        return self._parse_media(url)
+
+    def _parse_media(self, url: URL) -> MediaAlbum | MediaFile:
         media_type = url.parts[1]
         match media_type:
             case "a":
-                soup = get_soup(self.http_client.get_response(url).text)
-                return MediaAlbum(
-                    title=parse_title(soup),
-                    url=url,
-                    file_urls=parse_file_urls(soup, "#table .image", url.origin()),
-                )
+                return self._parse_album(url)
             case "f":
-                api = self.BASE_API.joinpath(url.path[1:])
-                json = self.http_client.get_response(api).json()
-                return MediaFile(
-                    title=json["name"],
-                    filename=parse_filename(json["name"]),
-                    url=url,
-                    stream_url=json["url"],
-                )
+                return self._parse_file(url)
             case _:
                 raise UndefinedMediaTypeError(url, media_type)
+
+    def _parse_album(self, url: URL) -> MediaAlbum:
+        soup = get_soup(self.http_client.get_response(url).text)
+        return MediaAlbum(
+            title=parse_title(soup),
+            url=url,
+            file_urls=parse_file_urls(soup, "#table .image", url.origin()),
+        )
+
+    def _parse_file(self, url: URL) -> MediaFile:
+        api = self.BASE_API.joinpath(url.path[1:])  # [1] is "/"
+        json = self.http_client.get_response(api).json()
+        return MediaFile(
+            title=json["name"],
+            filename=parse_filename(json["name"]),
+            url=url,
+            stream_url=json["url"],
+        )
